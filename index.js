0450b407622ec2938a5e741116446541baf2eaf6
Mogamat Nur Ally <92162895+Mono-Ally@users.noreply.github.com>
2022-07-21 14:29:56 +0000

    added moment dependancy and functions

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 // init project
 var express = require('express');
 var app = express();
-
+var moment = require('moment');
+moment().format(); 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,7 +25,28 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/:date",(req,res,next)=>{
+  let date = req.params.date;
+//unix validity checker
+  if (moment(date, 'X', true).isValid()){
+    res.json({
+    unix:date,
+      utc: moment(date, 'X', true).format()
+  })
+      } 
+    //utc validity checker
+  else if (moment(date, 'YYYY-MM-DD', true).isValid()){
+    res.json({
+    unix:moment(date,'YYYY-MM-DD',true).format().unix(),
+      utc: moment(date, 'YYYY-MM-DD', true).format()
+  })
+  }
+  else {
+    res.json({
+    error:"Invalid Date"
+  })
+  }
+  });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
